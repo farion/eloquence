1132ca7ea4615211ef55fd5b359ea57a40ed4caa
Frieder Reinhold <reinhold@trigon-media.com>
2016-08-04 11:49:50 +0200

    added hockey app, fixed XEP-0313 for iOS, added setting to clear local history

---
+++ Eloquence/EloquenceIOS/EMPreferencesButtonCell.swift
@@ -0,0 +1,13 @@
+//
+//  EMPreferencesButtonCell.swift
+//  Eloquence
+//
+//  Created by Frieder Reinhold on 03.08.16.
+//  Copyright © 2016 TRIGONmedia. All rights reserved.
+//
+
+import Cocoa
+
+class EMPreferencesButtonCell: NSObject {
+
+}
